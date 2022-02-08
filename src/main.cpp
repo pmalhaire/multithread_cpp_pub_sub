46891456ffebc6ccb89b0387a6cf30dcbcaef5dc
Pierre Malhaire <pierre.malhaire@stereolabs.com>
2022-02-08 11:52:41 +0100

    add mem test

--- src/main.cpp
+++ src/main.cpp
@@ -10,22 +10,22 @@ using namespace std::chrono_literals;
 
 int main()
 {
-    // std::cout << "# Test subscriber" << std::endl;
-    // testSub(1,1,0ms);
-    // testSub(1,1,10ms);
-    // testSub(10,10,10ms);
-    // testSub(1000,1000,0ms);
+    std::cout << "# Test subscriber" << std::endl;
+    testSub(1,1,0ms);
+    testSub(1,1,10ms);
+    testSub(10,10,10ms);
+    testSub(1000,1000,0ms);
 
-    // std::cout << std::endl << "# Test publisher" << std::endl;
-    // testPub(1,1,0ms);
-    // testPub(1,1,10ms);
-    // testPub(10,10,10ms);
-    // testPub(1000,1000,0ms);
+    std::cout << std::endl << "# Test publisher" << std::endl;
+    testPub(1,1,0ms);
+    testPub(1,1,10ms);
+    testPub(10,10,10ms);
+    testPub(1000,1000,0ms);
 
     std::cout << std::endl << "# Test publisher subscriber" << std::endl;
     testPubSub(1,1,1ms,100ms);
     testPubSub(1,1,10ms,100ms);
     testPubSub(10,10,1ms,100ms);
-    testPubSub(1000,1000,0ms,1000ms);
-    testPubSub(10,1000,0ms,1000ms,1000000);
+    testPubSub(1000,1000,0ms,100ms);
+    testPubSub(20,1000,0ms,10ms,2000000);
 }
